314762d2ef17f1776c33b9f71a73e35d6e26d38e
habilithm97 <habinkim97@gmail.com>
2022-05-11 12:57:43 +0900

    그림판 UI 구현

--- app/src/main/java/com/example/paintmaster/MainActivity.java
+++ app/src/main/java/com/example/paintmaster/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.paintmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    SingleTouchView singleTouchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        singleTouchView = (SingleTouchView)findViewById(R.id.singleTouchView);
+
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paintColor);
+        LinearLayout paintLayout2 = (LinearLayout)findViewById(R.id.paintColor2);
     }
 }
